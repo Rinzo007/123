c086c0d5fe00745fba4de6ae001a50a510208b16
Rinzo007 <62759866+Rinzo007@users.noreply.github.com>
2026-09-21 01:10:39 +0300

    p3: expose first Takt route decision for differential debugging

--- scripts/takt_browser_snapshot.js
+++ scripts/takt_browser_snapshot.js
@@ -72,10 +72,14 @@ sandbox.self = {
   postMessage() {},
 };
 
+const debugNeedle = "const Xt=at*k+G;";
+const debugIndex = bundle.indexOf(debugNeedle, bundle.indexOf("async function Bs("));
+if (debugIndex < 0) throw new Error("Could not locate Takt assignment decision marker");
+const debugCode = `\nif (H === 0 && at === 0 && G === 0) globalThis.__TAKT_debug = { ds: ds[0], Ge: Ge[0], rn: rn[0], ride: no[0], crowd: oo[0], transferWait: ro[0], transfer: ao[0], co: co(bt[0].legs[0], 0, G), btS: bt[0].s, Lr: Lr(bt[0].legs[0], 0), legs: bt[0].legs };\n`;
 const instrumented =
-  bundle.slice(0, markerIndex) +
-  "\nglobalThis.__TAKT_Bs = Bs;\n" +
-  bundle.slice(markerIndex);
+  bundle.slice(0, debugIndex) +
+  debugCode +
+  bundle.slice(debugIndex).replace("\\n" + "globalThis.__TAKT_Bs = Bs;" + "\\n", "\\n" + "globalThis.__TAKT_Bs = Bs;" + "\\nglobalThis.__TAKT_Bs_debug = () => globalThis.__TAKT_debug;" + "\\n", 1);
 
 vm.runInNewContext(instrumented, sandbox, {
   filename: bundlePath,
@@ -143,7 +147,7 @@ async function main() {
   );
 
   const line = result.lines?.[0];
-  const snapshot = {
+  const snapshot = {\n    debug: sandbox.__TAKT_debug ?? null,
     reference: {
       engine: "Takt web bundle",
       bundle: "bd956ff0a1875604740f.js",
