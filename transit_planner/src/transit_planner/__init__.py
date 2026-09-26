f9e0f7b0828008cd1a9f713d716eb47583ad220c
Rinzo007 <62759866+Rinzo007@users.noreply.github.com>
2026-09-26 20:14:03 +0300

    Export Overture topology APIs

--- transit_planner/src/transit_planner/__init__.py
+++ transit_planner/src/transit_planner/__init__.py
@@ -16,10 +16,24 @@ from .assignment import (
     assign_demand,
 )
 from .city import City, DemandZone
+from .data import ConnectorRecord, ConnectorRef, RoadRecord
 from .demand import DemandMatrix, ODPairDemand
 from .economics import EconomicsConfig, EconomicsResult, calculate_economics
 from .network import Network
 from .od import GravityParameters, gravity_od
+from .overture import (
+    DEFAULT_RELEASE,
+    OvertureConnectorProvider,
+    OvertureSource,
+    OvertureTransitProvider,
+    OvertureTransportationProvider,
+)
+from .road import RoadEdge, RoadGraph, RoadNode
+from .road_builder import (
+    RoadGraphBuildResult,
+    build_road_graph,
+    build_topological_road_graph,
+)
 from .routing import Journey, JourneyLeg, RouterConfig, TransitRouter
 from .scenario import (
     MetricDelta,
@@ -33,15 +47,54 @@ from .scenario import (
 from .simulation import SimulationResult, simulate_capacity
 
 __all__ = [
-    "AccessibilityResult", "NetworkAnalytics", "SectionAnalytics", "StopAnalytics",
-    "analyze_network", "calculate_accessibility",
-    "AssignmentConfig", "AssignmentMetrics", "AssignmentResult",
-    "RouteFlow", "SectionLoad", "StopFlow", "assign_demand",
-    "City", "DemandZone", "DemandMatrix", "ODPairDemand",
-    "EconomicsConfig", "EconomicsResult", "calculate_economics",
-    "Network", "GravityParameters", "gravity_od",
-    "Journey", "JourneyLeg", "RouterConfig", "TransitRouter",
-    "MetricDelta", "ScenarioComparison", "ScenarioDefinition", "ScenarioRun",
-    "SectionDelta", "compare_scenarios", "run_scenario",
-    "SimulationResult", "simulate_capacity",
+    "AccessibilityResult",
+    "NetworkAnalytics",
+    "SectionAnalytics",
+    "StopAnalytics",
+    "analyze_network",
+    "calculate_accessibility",
+    "AssignmentConfig",
+    "AssignmentMetrics",
+    "AssignmentResult",
+    "RouteFlow",
+    "SectionLoad",
+    "StopFlow",
+    "assign_demand",
+    "City",
+    "DemandZone",
+    "ConnectorRecord",
+    "ConnectorRef",
+    "RoadRecord",
+    "DemandMatrix",
+    "ODPairDemand",
+    "EconomicsConfig",
+    "EconomicsResult",
+    "calculate_economics",
+    "Network",
+    "GravityParameters",
+    "gravity_od",
+    "DEFAULT_RELEASE",
+    "OvertureConnectorProvider",
+    "OvertureSource",
+    "OvertureTransitProvider",
+    "OvertureTransportationProvider",
+    "RoadEdge",
+    "RoadGraph",
+    "RoadNode",
+    "RoadGraphBuildResult",
+    "build_road_graph",
+    "build_topological_road_graph",
+    "Journey",
+    "JourneyLeg",
+    "RouterConfig",
+    "TransitRouter",
+    "MetricDelta",
+    "ScenarioComparison",
+    "ScenarioDefinition",
+    "ScenarioRun",
+    "SectionDelta",
+    "compare_scenarios",
+    "run_scenario",
+    "SimulationResult",
+    "simulate_capacity",
 ]
